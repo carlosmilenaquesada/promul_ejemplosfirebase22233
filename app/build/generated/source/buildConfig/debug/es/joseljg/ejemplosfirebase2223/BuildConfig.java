29a730462c69be6e0ca8a693bc36a38558fa7036
carlosmilenaquesada_portatil <carlosmilenaquesada@gmail.com>
2023-11-27 00:58:19 +0100

    ultimo

---
+++ app/build/generated/source/buildConfig/debug/es/joseljg/ejemplosfirebase2223/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package es.joseljg.ejemplosfirebase2223;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "es.joseljg.ejemplosfirebase2223";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
